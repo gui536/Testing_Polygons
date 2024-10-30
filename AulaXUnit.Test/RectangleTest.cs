9006ad362156b31b2d4cc00e580d45c55ca2502e
Guilherme Santos <guilherme.santos536@etec.sp.gov.br>
2024-10-30 20:20:25 -0300

    feat: Added RectangleTest. Sample class for unit Testing in C# using XUnit

---
+++ AulaXUnit.Test/RectangleTest.cs
@@ -0,0 +1,16 @@
+﻿using AulaXUnit;
+
+namespace AulaXUnit.Test
+{
+    public class RectangleTest
+    {
+        [Fact]
+        public void TestArea()
+        {
+            AulaXUnit.Rectangle rectangle = new AulaXUnit.Rectangle(2, 3);
+            double ExpectedArea = 6;
+            double ResultArea = rectangle.getArea();
+            Assert.Equal(ExpectedArea, ResultArea);
+        }
+    }
+}
---
+++ AulaXUnit/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿namespace AulaXUnit
+{
+    public class Rectangle
+    {
+        public double[] Sides { get; set; }
+        public double Area { get; set; }
+        public double Perimeter { get; set; }
+
+        public Rectangle(double a, double b)
+        {
+            Sides = new double[4];
+            Sides[0] = a;
+            Sides[1] = a;
+            Sides[2] = b;
+            Sides[3] = b;
+            Array.Sort(Sides);
+        }
+
+        public double getArea()
+        {
+            //Retorna a área de um retângulo.
+            return Sides[0] * Sides[3];
+        }
+
+        public double getPerimeter()
+        {
+            return (Sides[0] + Sides[1] + Sides[2] + Sides[3]);
+        }
+    }
+
+}
