518597cf3fafd753b3c5ee7ab6da8d2f0a57b95d
Guilherme Santos <guilherme.santos536@etec.sp.gov.br>
2024-11-13 16:26:24 -0300

    feat: Added new tests

--- AulaXUnit.Test/RectangleTest.cs
+++ AulaXUnit.Test/RectangleTest.cs
@@ -12,5 +12,29 @@ namespace AulaXUnit.Test
             double ResultArea = rectangle.getArea();
             Assert.Equal(ExpectedArea, ResultArea);
         }
+        [Fact]
+        public void TestNullArea()
+        {
+            AulaXUnit.Rectangle rectangle = new AulaXUnit.Rectangle(0, 0);
+            double ExpectedArea = 0;
+            double ResultArea = rectangle.getArea();
+            Assert.Equal(ExpectedArea, ResultArea);
+        }
+        [Fact]
+        public void TestPerimeter()
+        {
+            AulaXUnit.Rectangle rectangle = new AulaXUnit.Rectangle(2, 3);
+            double ExpectedPerimeter = 10;
+            double ResultPerimeter = rectangle.getPerimeter();
+            Assert.Equal(ExpectedPerimeter, ResultPerimeter);
+        }
+        [Fact]
+        public void TestNullPerimeter()
+        {
+            AulaXUnit.Rectangle rectangle = new AulaXUnit.Rectangle(0, 0);
+            double ExpectedArea = 0;
+            double ResultPerimeter = rectangle.getArea();
+            Assert.Equal(ExpectedArea, ResultPerimeter);
+        }
     }
 }
